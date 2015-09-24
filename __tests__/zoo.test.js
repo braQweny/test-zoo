d14fbdaf2a74bec1eb6ebfdd4d4e61c3f518db7f
Pawel <brakweny@gmail.com>
2015-09-24 13:52:22 +0200

    add

--- __tests__/zoo.test.js
+++ __tests__/zoo.test.js
@@ -16,34 +16,51 @@ zoo.add(new Elephant('Bożena'));
 describe('zoo', function () {
 
     it('inherit from Animal', function () {
+
         for (var animal of zoo) {
+
             expect(Animal.prototype.isPrototypeOf(animal)).toBeTruthy();
             expect(animal instanceof Animal).toBeTruthy();
+
         }
+        
     });
 
     it('overwrite method getRoar', function () {
+
         expect(Elephant.prototype.hasOwnProperty('getRoar')).toBeTruthy();
         expect(Lion.prototype.hasOwnProperty('getRoar')).toBeTruthy();
+
         for (var x of zoo) {
             expect(x.getRoar()==='xxxxxxxxxxx'||x.getRoar()==='####################').toBeTruthy();
         };
+
     });
 
     it('has own name', function () {
+
         for (var animal of zoo) {
+
             expect(animal.hasOwnProperty('name')).toBeTruthy();
-            expect(animal.name).toBeDefined()
+            expect(animal.name).toBeDefined();
+
         }
+
     });
 
     it('!(getRoar implementation)', function () {
+
         var testAnimal = new Animal();
+
         try{
+
             testAnimal.getRoar();
+
         }
         catch(e){
+
             expect(e.message).toBe('lack implemented roar');
+
         }
     });
 
